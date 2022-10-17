57f0c3e2af744ebc3e99367f88536ec18b7181fd
fitsumM12 <fitsummesfindejene@gmail.com>
2022-10-17 00:39:11 -0700

    update

--- sort.h
+++ sort.h
@@ -25,6 +25,14 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 /* QuickSort helper functions */
 void quicker_sort(int *array, size_t size, int lo, int hi);
 int partition(int *array, size_t size, int lo, int hi);
